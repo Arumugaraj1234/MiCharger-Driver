46a8b157079f6a2b4cf2ee26ed8b54635335892d
MacBook Pro <macbookpro@macbooks-macbook-pro.local>
2019-11-25 09:54:32 +0530

    Initial Commit

---
+++ MiCharger/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  MiCharger
+//
+//  Created by MacBook Pro on 2019-11-25.
+//  Copyright © 2019 Peach. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ MiCharger/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  MiCharger
+//
+//  Created by MacBook Pro on 2019-11-25.
+//  Copyright © 2019 Peach. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
